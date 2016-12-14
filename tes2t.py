38b9585691198b95d1713199292d4dad93a3e9e4
6yyy <6yyy@mail.ru>
2016-12-14 18:31:26 +0700

    check all site url

--- tes2t.py
+++ tes2t.py
@@ -5,12 +5,13 @@ import json
 import smtplib
 
 
+
 def send_email(mes):
     sender = 'polozov@intrice.ru'
     receivers = ['polozov@intrice.ru']
     message = """From: From Person <polozov@intrice.ru>
 To: To Person <polozov@intrice.ru>
-Subject: selenide-web-test
+Subject: test
 
 selenide-web-test.
 """
@@ -19,37 +20,49 @@ selenide-web-test.
     smtpObj.sendmail(sender, receivers, message)
     print("Successfully sent email")
 
+#def get_page(url):
+#	return urllib.request.urlopen(url).read().decode("utf-8")
 
-
-add_result = []
-add_result_error = []
-
-def get_page(url):
-	return urllib.request.urlopen(url).read().decode("utf-8")
-
-def get_json(page):
-	urlPageJson = json.loads(page)
+def get_json(url):
+	urlPageJson = json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
 	return urlPageJson
 
+url_env_auto = "http://hudson.connectivegames.com/view/%7Benv-auto%7D/api/json"
+url_st = "http://hudson.connectivegames.com/view/st/api/json"
+url_st_dev = "http://hudson.connectivegames.com/view/st-dev/api/json"
 url_st_web = "http://hudson.connectivegames.com/view/st-web/api/json"
+url_mobile_mac = "http://hudson.connectivegames.com/view/%7Bmobile%20+%20mac%7D/api/json"
+url_win32_SM = "http://win32builder.intrice.ru:8080/api/json"
+url_lib = [url_env_auto,url_st,url_st_dev,url_st_web,url_mobile_mac,url_win32_SM]
 
-home_page = get_page(url_st_web)
-json_home_page = get_json(home_page)
-for item in json_home_page["jobs"]:
-	if item.get("color") != "disabled":
-		build_page = get_page(item.get("url")+"lastBuild/api/json")
-		build_json = get_json(build_page)
-        if build_json["result"] == "UNSTABLE":
-            build_page_error = get_page(item.get("url") + "lastBuild/testReport/junit/com.connectivegames.sites.utils/SelenideWebTest/api/json")
-            build_json_error = get_json(build_page_error)
-            for item_error in build_json_error["child"]:
-                add_result_error += item_error.get("name") + "  \n"
-		if build_json["result"] != "SUCCESS":
-				add_result += ([build_json["fullDisplayName"] + build_json["result"]])
-print(add_result)
 
-y += add_result+"\n"+url + job_name + "lastBuild\n" + add_result_error+"\n"
+def get_error(url_site):
+    add_result = str()
+    add_result_error = str()
+    send_enstable = str()
+    send_failure = str()
+    json_home_page = get_json(url_site)
+    for item in json_home_page["jobs"]:
+        if item.get("color") != "disabled":
+            build_json = get_json((item.get("url")+"lastBuild/api/json"))
+            if build_json["result"] == "UNSTABLE":
+                build_json_error = get_json((item.get("url") + "lastBuild/testReport/api/json"))
+                add_result_error += item.get("url")+str(build_json["number"])+"\n"
+                for item_error in build_json_error["suites"][0]["cases"]:
+                    if item_error.get("status") == "REGRESSION":
+                        add_result_error += item_error.get("name") + "  \n"
+            elif build_json["result"] == "FAILURE":
+                add_result += (item.get("url")+str(build_json["number"])+ "\n" +"  " + str(build_json["result"]) +"\n")
 
-print(y)
+    send_enstable += "\n" + add_result_error + "\n"
+    send_failure += "\n" + str(add_result) + "\n"
 
-#send_email(y)
+    if len(add_result) > 0:
+        send_email(send_failure)
+        print(send_failure)
+    if len(add_result_error) > 0:
+        send_email(send_enstable)
+        print(send_enstable)
+
+for item in url_lib:
+   get_error(item)
